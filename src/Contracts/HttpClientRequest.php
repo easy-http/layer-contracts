cbace0071e4ca42582989c8558472b321b59908a
Darío Rivera <fermius.us@gmail.com>
2022-03-27 20:37:12 -0500

    docs: add php docblock for ssl

--- src/Contracts/HttpClientRequest.php
+++ src/Contracts/HttpClientRequest.php
@@ -14,5 +14,13 @@ interface HttpClientRequest
     public function setHeader(string $key, string $value): self;
     public function setJson(array $json): self;
     public function setQuery(array $query): self;
+
+    /**
+     * SSL Client Verification
+     * We suggest this parameter will be false by default.
+     *
+     * @param bool $ssl
+     * @return void
+     */
     public function ssl(bool $ssl): void;
 }
