70814a2e16523eb86786a3490dab9a29da897c45
Darío Rivera <fermius.us@gmail.com>
2022-04-07 21:43:47 -0500

    tests: make base test case abstract

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,7 @@ namespace EasyHttp\LayerContracts\Tests;
 use Faker\Factory;
 use Faker\Generator;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected Generator $faker;
 
